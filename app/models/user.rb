d521a7f6507e77ab296ea60efba90b436aae0fe3
Aurelio Grott <aurelio.neto@trustedhealth.com>
2023-11-17 23:31:26 -0300

    fix rubocop inconsistencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# User base class for product take home assignment
+# Has only email and password attributes
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
