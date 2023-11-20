d30fe76069391ea76e74722b202b61f37c98e63f
Aurelio Grott <aurelio.neto@trustedhealth.com>
2023-11-20 11:15:21 -0300

    add spec

--- spec/models/user.rb
+++ spec/models/user.rb
@@ -6,9 +6,16 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     let(:user) { create(:user) }
 
-    it 'requires the presence of an email address' do
-      user.email = nil
-      expect(user).not_to be_valid
+    context 'email address validation' do
+      it 'requires the presence of an email address' do
+        user.email = nil
+        expect(user).not_to be_valid
+      end
+
+      it 'requires a valid email address' do
+        user.email = 'invalid_email.com'
+        expect(user).not_to be_valid
+      end
     end
 
     it 'requires the presence of a password' do
