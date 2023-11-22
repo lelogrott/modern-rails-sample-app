4aeacd3ffdb476fc3ee2bfbc94097d193fa92bae
Aurelio Grott <aurelio.neto@trustedhealth.com>
2023-11-22 15:34:20 -0300

    prevent submit button from disabling when preview is triggered

--- app/javascript/controllers/autosubmit_controller.js
+++ app/javascript/controllers/autosubmit_controller.js
@@ -2,15 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="autosubmit"
 export default class extends Controller {
-  static targets = ["submitbutton"]
+  static targets = ["previewbutton"]
   connect() {
-    this.submitbuttonTarget.hidden = true;
+    this.previewbuttonTarget.hidden = true;
   }
   submit() {
     // debounce strategy for autosubmit form
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
-      this.submitbuttonTarget.click();
+      this.previewbuttonTarget.click();
     }, 500);
   }
 }
